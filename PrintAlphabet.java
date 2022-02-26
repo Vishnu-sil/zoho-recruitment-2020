2f1fbf8bf1905278d9ee6709e0dbffca767a6450
Vishnu <100416692+Vishnu-sil@users.noreply.github.com>
2022-02-26 12:10:40 +0530

    Add files via upload
    Pattern generator:
    Input: 
    n=5
    Output:
    A
    B F
    C G J 
    D H K M
    E I L N O
    
    In case, the pattern exceeds more than 26 characters fill those with '*'
    Input:
    n=8
    
    Output:
    A
    B I
    C J P
    D K Q V
    E L R W *
    F M S X * * 
    G N T Y * * *
    H O U Z * * * *

---
+++ PrintAlphabet.java
@@ -0,0 +1,40 @@
+package solutions;
+
+import java.util.Scanner;
+
+public class PrintAlphabet {
+
+	public static void main(String[] args) {
+	
+		/* Output Pattern: 
+		A 
+		B I 
+		C J P 
+		D K Q V 
+		E L R W * 
+		F M S X * * 
+		G N T Y * * * 
+		H O U Z * * * * 
+		*/
+		
+		Scanner sc=new Scanner(System.in);
+		
+		System.out.println("Enter the value to Generate pattern: ");
+		int num=sc.nextInt(); // Input num - Get the number of row to generate a pattern
+		int inc; // int Inc- increment pattern logic
+		
+		for(int i=1;i<=num;i++) {
+			
+			inc=i;
+			
+			for(int j=1;j<=i;j++) {
+				if(inc<=26) System.out.print((char) ((char)inc+64)+" "); // Convert the int to char ASCII respective character 
+				else System.out.print("*"+" ");							 // Print '*' pattern exceeds more than 26 characters
+				inc=inc+(num-j); 
+			}
+			
+			System.out.println();
+		}
+	}
+
+}
